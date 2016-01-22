d31221d210aa34fdb57b811d8262ac72b95ea781
Steve Shaffer <steve@shaffer.tech>
2016-01-21 23:30:00 -0800

    Add created_at and updated_at attributes to base model

--- server/core/handlers.py
+++ server/core/handlers.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 # TODO: Better module name than "handlers"
+from datetime import datetime
+import json
+
 import webapp2
 
-import json
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code
+
+    :param obj: The object to be serialized
+    """
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError("Type not serializable")
 
 
 class BaseHandler(webapp2.RequestHandler):
 
     model = None  # Override with model class  # TODO: Should this default to a generic BaseModel class?
 
-    def send_json(self, content):
+    def send_json(self, data):
         self.response.headers['content-type'] = 'application/json'
-        self.response.write(json.dumps(content))  # TODO: Need to be able to parse dates (and other types I'm sure)
+        self.response.write(json.dumps(data, default=json_serial))
+        # TODO: Need to be able to parse dates (and other types I'm sure)
 
 
 class CollectionHandler(BaseHandler):
--- server/core/models.py
+++ server/core/models.py
@@ -4,6 +4,9 @@ from google.appengine.ext import ndb
 
 class BaseModel(ndb.Model):
 
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
+    updated_at = ndb.DateTimeProperty(auto_now=True)
+
     @classmethod
     def api_list(cls):
         return [obj.api_message() for obj in cls.query()]  # TODO: Expensive.  Optimize, paginate, etc.
@@ -12,7 +15,21 @@ class BaseModel(ndb.Model):
     def api_read(cls, *args):
         return cls.get_by_id(args[0]).api_message()
 
-    def api_message(self):  # TODO: Allow thing to be added, not just overridden?
-        return {
-            'id': self.key.id()
-        }
+    def api_message(self, stub=False):  # TODO: Allow thing to be added, not just overridden?
+        """Define the message sent back for a READ request from the API
+
+        Override this to redefine what gets sent or call this with super and subclass it
+        # TODO: Use _api_message() instead of requiring super?
+
+        Args:
+            stub: Should we just send a quick stub of the data? (useful for collections)
+
+        Returns: Data to be passed back through the API
+        """
+        data = {'id': self.key.id()}
+        if not stub:
+            data.update({
+                'created_at': self.created_at,
+                'updated_at': self.updated_at
+            })
+        return data
